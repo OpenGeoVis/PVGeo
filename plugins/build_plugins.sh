4a96a6fc8c78d70a95a8bc549b872e524448002f
Bane Sullivan <chrsulli@mines.edu>
2017-08-07 15:15:45 -0600

    Updated during development.

--- plugins/build_plugins.sh
+++ plugins/build_plugins.sh
@@ -5,7 +5,17 @@
 python python_filter_generator.py filters_source_code/filter_table_to_image_data.py xml_plugins/table_to_image_data.xml
 
 # Flip ImageData Axis Filter
-python python_filter_generator.py filters_source_code/filter_flip_image_data_axis.py xml_plugins/flip_image_data_axis.xml
+python python_filter_generator.py filters_source_code/filter_flip_image_data_axii.py xml_plugins/filter_flip_image_data_axii.xml
+
+# filter_unstructured_points_to_image_data
+python python_filter_generator.py filters_source_code/filter_unstructured_grid_maker.py xml_plugins/filter_unstructured_grid_maker.xml
+
+# Time to Space stuff
+python python_filter_generator.py filters_source_code/filter_time_to_space.py xml_plugins/filter_time_to_space.xml
+
+# Reshape Table
+python python_filter_generator.py filters_source_code/filter_reshape_table.py xml_plugins/filter_reshape_table.xml
+
 
 
 #------ READERS ------#
@@ -17,3 +27,5 @@ python python_filter_generator.py readers_source_code/read_delimited_file_to_tab
 
 # Read GEO EAS Files To Table
 python python_filter_generator.py readers_source_code/read_geoeas_file_to_table.py xml_plugins/read_geoeas_file_to_table.xml
+
+sh ./install_plugins.sh
