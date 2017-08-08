8624b17f884a08e92eb8cd27f32a1afd318be363
Bane Sullivan <chrsulli@mines.edu>
2017-08-08 08:18:58 -0600

    For Jacob: this filter takes vtkPolyData with point data and builds a tube (tunnel) around those points at a given radius. The radius and number of sides of the tube can be specified by user.

---
+++ plugins/filters_source_code/filter_tube_from_points.py
@@ -0,0 +1,40 @@
+Name = 'TubeFromPoints'
+Label = 'Tube From Points'
+Help = 'This takes points from a vtkPolyData object and constructs a line of those points then builds a tube around that line.'
+
+NumberOfInputs = 1
+InputDataType = 'vtkPolyData'
+OutputDataType = 'vtkPolyData'
+ExtraXml = '''\
+<Hints>
+    <ShowInMenu category="CSM Geophysics Filters" />
+</Hints>
+'''
+
+
+Properties = dict(
+    Number_of_Sides=20,
+    Radius=1.0,
+    Input_Arr=3,
+)
+
+
+def RequestData():
+    pdi = self.GetInput() # VTK PolyData Type
+    pdo = self.GetOutput() # VTK PolyData Type
+
+    numPoints = pdi.GetNumberOfPoints()
+    pdo.Allocate()
+
+    for i in range(0, numPoints-1):
+        points = [i, i+1]
+        # VTK_LINE is 3 -> TODO:
+        pdo.InsertNextCell(Input_Arr, 2, points)
+
+    # Make a tube from the PolyData line:
+    tube = vtk.vtkTubeFilter()
+    tube.SetInputData(pdo)
+    tube.SetRadius(10)
+    tube.SetNumberOfSides(20)
+    tube.Update()
+    pdo.ShallowCopy(tube.GetOutput())
