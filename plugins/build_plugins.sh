91bbad48e08bf690dab2706936afb63287826888
Bane Sullivan <chrsulli@mines.edu>
2017-08-26 16:07:27 -0600

    changed to explicitly use python2

--- plugins/build_plugins.sh
+++ plugins/build_plugins.sh
@@ -3,19 +3,19 @@
 #------ FILTERS ------#
 for filename in ./filters_source_code/filter_*.py; do
     filtername="${filename%.*}"
-    python python_filter_generator.py $filename "./xml_plugins/$(basename "$filtername").xml"
+    python2 python_filter_generator.py $filename "./xml_plugins/$(basename "$filtername").xml"
 done
 
 #------ SOURCES ------#
 for filename in ./artificial_sources_source_code/create_*.py; do
     filtername="${filename%.*}"
-    python python_filter_generator.py $filename "./xml_plugins/$(basename "$filtername").xml"
+    python2 python_filter_generator.py $filename "./xml_plugins/$(basename "$filtername").xml"
 done
 
 #------ READERS ------#
 for filename in ./readers_source_code/read_*.py; do
     filtername="${filename%.*}"
-    python python_filter_generator.py $filename "./xml_plugins/$(basename "$filtername").xml"
+    python2 python_filter_generator.py $filename "./xml_plugins/$(basename "$filtername").xml"
 done
 
 #------ INSTALL ------#
