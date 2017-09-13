3c8482ba90548ca87bf5c27486406c3c3b7a5369
Bane Sullivan <chrsulli@mines.edu>
2017-09-12 23:47:50 -0600

    Renamed directories for consistent and simple organization.

--- src/build_plugins.sh
+++ src/build_plugins.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 #------ WRAP FILTERS IN XML ------#
-for filename in ./filters_source_code/filter_*.py; do
+for filename in ./filters/filter_*.py; do
     filtername="${filename%.*}"
     python2 python_filter_generator.py $filename "../build/$(basename "$filtername").xml"
 done
 
 #------ WRAP SOURCES IN XML ------#
-for filename in ./artificial_sources_source_code/create_*.py; do
+for filename in ./artificial_sources/create_*.py; do
     filtername="${filename%.*}"
     python2 python_filter_generator.py $filename "../build/$(basename "$filtername").xml"
 done
 
 #------ WRAP READERS IN XML ------#
-for filename in ./readers_source_code/read_*.py; do
+for filename in ./readers/read_*.py; do
     filtername="${filename%.*}"
     python2 python_filter_generator.py $filename "../build/$(basename "$filtername").xml"
 done
