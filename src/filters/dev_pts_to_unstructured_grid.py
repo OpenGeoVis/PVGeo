90e0637955a80fa63b75847f957ec9cbfdb19127
Bane Sullivan <chrsulli@mines.edu>
2017-09-19 01:41:48 -0600

    Devloping filter to generate a grid from scattered points.

---
+++ src/filters/dev_pts_to_unstructured_grid.py
@@ -0,0 +1,45 @@
+Name = 'PointsTovtkUnstructuredGrid'
+Label = 'Points To vtkUnstructuredGrid'
+Help = 'Input is vtkPolyData (from Table to Points Filter)'
+
+NumberOfInputs = 1
+InputDataType = 'vtkPolyData'
+OutputDataType = 'vtkUnstructuredGrid'
+ExtraXml = '''\
+<Hints>
+    <ShowInMenu category="CSM Geophysics Filters DEV" />
+</Hints>
+'''
+
+Properties = dict(
+
+)
+
+
+def RequestData():
+    pdi = self.GetInput()  # vtkPolyData -> from Table to Points Filter
+    grid = self.GetOutput()  # vtkUnstructuredGrid
+
+    #print(pdi.GetPoints())
+
+    Ids = vtk.vtkIdList()
+    for i in range(pdi.GetPoints().GetNumberOfPoints()):
+        Ids.InsertNextId(i)
+
+
+    grid.Allocate(1,1)
+    grid.InsertNextCell(12, Ids)
+
+    grid.SetPoints(pdi.GetPoints())
+
+    # Add all columns of the table as arrays to the PointData
+    for i in range(pdi.GetPointData().GetNumberOfArrays()):
+        arr = pdi.GetPointData().GetArray(i)
+        grid.GetPointData().AddArray(arr)
+
+    grid.Squeeze()
+
+def RequestInformation():
+    from paraview import util
+    # ABSOLUTELY NECESSARY FOR THE FILTER TO WORK:
+    #util.SetOutputWholeExtent(self, [0,nx-1,0,ny-1,0,nz-1])
