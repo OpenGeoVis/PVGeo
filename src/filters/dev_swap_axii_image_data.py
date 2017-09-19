3c9a530dd7fa7d4d393894127b10af6f8313a1b6
Bane Sullivan <chrsulli@mines.edu>
2017-09-19 01:41:26 -0600

    Developing filter to transpose image data axii. (uniform grid)

---
+++ src/filters/dev_swap_axii_image_data.py
@@ -0,0 +1,56 @@
+Name = 'SwapAxiiImageData'
+Label = 'Swap Axii Image Data'
+Help = ''
+
+NumberOfInputs = 1
+InputDataType = 'vtkImageData'
+OutputDataType = 'vtkImageData'
+ExtraXml = '''\
+<Hints>
+    <ShowInMenu category="CSM Geophysics Filters DEV" />
+</Hints>
+
+<IntVectorProperty
+    name="Axii"
+    command="SetParameter"
+    number_of_elements="1"
+    initial_string="test_drop_down_menu"
+    default_values="0">
+    <EnumerationDomain name="enum">
+          <Entry value="0" text="Swap X and Y axii"/>
+          <Entry value="1" text="Swap X and Z axii"/>
+          <Entry value="2" text="Swap Y and Z axii"/>
+    </EnumerationDomain>
+    <Documentation>
+        This property indicates which two axii will be swapped.
+    </Documentation>
+</IntVectorProperty>
+'''
+
+Properties = dict(
+    Axii=0,
+)
+
+def RequestData():
+    pdi = self.GetInput()
+    pdo = self.GetOutput() #vtkImageData
+
+    [nx,ny,nz] = pdi.GetDimensions()
+
+    print(Axii)
+
+    # Grab Data values
+    # Put into 3D structure
+    # Swap axis in that structure
+    # unfold
+    # convert to VTK arrays
+    # set name accordingly
+    # add array to pdo
+
+
+def RequestInformation():
+    from paraview import util
+    # ABSOLUTELY NECESSARY FOR THE FILTER TO WORK:
+    pdi = self.GetInput()
+    [nx,ny,nz] = pdi.GetDimensions()
+    util.SetOutputWholeExtent(self, [0,nx-1, 0,ny-1, 0,nz-1])
