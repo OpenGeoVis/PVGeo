f85090efbc91fcf3e842a95df9b31c36a1df5304
Bane Sullivan <chrsulli@mines.edu>
2017-10-22 14:07:03 -0600

    Comments/desciptions for shell scripts

--- updatePVGP.sh
+++ updatePVGP.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This is the update script for the repository for those that are unfimliar with
+#   using Git and do not want to mess with the scripts in this repo.
+
 pushd "$(dirname "$0")"
 
 # Pull from github
