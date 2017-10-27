dbe2b8d627d3ff1d771ecb8022cbf8de3617b9a1
Bane Sullivan <chrsulli@mines.edu>
2017-10-27 13:21:10 -0600

    Made the update a little more informative/pretty

--- updatePVGP.sh
+++ updatePVGP.sh
@@ -4,11 +4,41 @@
 
 pushd "$(dirname "$0")"
 
-# Pull from github
-git pull origin master
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+  RED="$(tput setaf 1)"
+  GREEN="$(tput setaf 2)"
+  YELLOW="$(tput setaf 3)"
+  BLUE="$(tput setaf 4)"
+  BOLD="$(tput bold)"
+  NORMAL="$(tput sgr0)"
+else
+  RED=""
+  GREEN=""
+  YELLOW=""
+  BLUE=""
+  BOLD=""
+  NORMAL=""
+fi
 
-pushd ./src
-sh ./clean_out.sh
-sh ./build_plugins.sh
-popd
-popd
+printf "${BLUE}%s${NORMAL}\n" "Updating ParaViewGeophysics..."
+
+if git pull --rebase --stat origin master
+then
+  printf "${GREEN}%s${NORMAL}\n" "Tsjakka!! ParaViewGeophysics has been updated and/or is at the current version."
+  printf "${BLUE}${BOLD}%s${NORMAL}\n" "To keep up on the latest news and updates, check out the Wiki Page on the GitHub Repo."
+  pushd ./src
+  sh ./clean_out.sh
+  sh ./build_plugins.sh
+  popd
+  popd
+else
+  printf "${RED}%s${NORMAL}\n" 'There was an error updating. Try again later?'
+fi
+
+# Pull from github
+#git pull origin master
