b0c066aaee41905d5a8503de7ffe324d0b1e650b
Bane Sullivan <chrsulli@mines.edu>
2017-11-08 10:22:24 -0700

    output formats and usage update

--- get_vtkjs_url.py
+++ get_vtkjs_url.py
@@ -17,6 +17,16 @@ For Example:
 import os
 import sys
 
+class stf:
+    """
+    String Formatting
+    """
+    G = '\033[92m'  # Green
+    R = '\033[91m'  # Red
+    B = '\033[1m'   # Bold
+    U = '\033[4m'   # Underline
+    END = '\033[0m' # Normal
+
 def convertDropboxURL(url):
     return url.replace("https://www.dropbox.com", "https://dl.dropbox.com")
 
@@ -31,7 +41,8 @@ def generateViewerURL(dataURL):
 
 def main():
     if len(sys.argv) != 3:
-        print('Usage: %s <web file host> <file link>' % sys.argv[0])
+        print("%s--> Incorrect arguments for the script!" % stf.R)
+        print('--> Usage: %s <web file host> <file link>%s' % (sys.argv[0], stf.END))
         sys.exit(1)
 
     host = sys.argv[1]
@@ -43,8 +54,9 @@ def main():
     elif host.lower() == "github":
         convertURL = convertGitHubURL(inURL)
     else:
+        print("%s%s--> Warning: Web host not specified or supported. URL is simply appended to standalone scene loader link.%s" % (stf.R, stf.B, stf.END))
         convertURL = inURL
-    print("Your link: %s" % generateViewerURL(convertURL))
+    print("--> Your link: %s%s%s%s" % (stf.U, stf.G, generateViewerURL(convertURL), stf.END))
     exit(0)
 
 if __name__ == '__main__':
