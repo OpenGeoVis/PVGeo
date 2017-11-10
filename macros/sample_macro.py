d9a71ffd21a57fa0eb704c5f6893ec9b1ddf6da6
Bane Sullivan <chrsulli@mines.edu>
2017-11-10 15:28:44 -0700

    Update arguments on export to work on all pythons

--- macros/sample_macro.py
+++ macros/sample_macro.py
@@ -1,4 +1,30 @@
 #! pvpython
 
+#### import the simple module from the paraview
+from paraview.simple import *
 from PVGPpy import *
-help(macros.clip_through)
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+# create a new 'Cone'
+cone1 = Cone()
+
+# get active view
+renderView1 = GetActiveViewOrCreate('RenderView')
+
+# show data in view
+cone1Display = Show(cone1, renderView1)
+# trace defaults for the display properties.
+cone1Display.Representation = 'Surface'
+
+# reset view to fit data
+renderView1.ResetCamera()
+
+# update the view to ensure updated data information
+renderView1.Update()
+
+
+#### uncomment the following to render all views and export
+RenderAllViews()
+export.exportVTKjs(FileName='test')
+# alternatively, if you want to write images, you can use SaveScreenshot(...).
