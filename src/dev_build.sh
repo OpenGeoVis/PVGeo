70434eae503b7b82054a2418a113633725727733
Bane Sullivan <chrsulli@mines.edu>
2017-11-09 20:55:53 -0700

    pritout for dev script. need to catch if no dev files present.

--- src/dev_build.sh
+++ src/dev_build.sh
@@ -34,5 +34,16 @@ for filename in ./filters/dev_*.py; do
     python2 python_filter_generator.py $filename "../build/$(basename "$filtername").xml"
 done
 printf "${NORMAL}"
+
+# readers under development:
+printf "${BLUE}${BOLD}%s${NORMAL}\n" "--> Attempting to wrap DEV READERS in XML..."
+for filename in ./readers/dev_*.py; do
+    filtername="${filename%.*}"
+    printf "${YELLOW}%s${NORMAL}\n" "    --> $(basename "$filtername")"
+    printf "${RED}" # Change printout color to red to signify errors
+    python2 python_filter_generator.py $filename "../build/$(basename "$filtername").xml"
+done
+printf "${NORMAL}"
+
 sh ./build_plugins.sh
 popd
