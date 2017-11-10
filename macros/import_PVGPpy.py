757e70446ad9308332a7fccb648f105dec71fb8d
Bane Sullivan <chrsulli@mines.edu>
2017-11-10 00:19:21 -0700

    fix import

--- macros/import_PVGPpy.py
+++ macros/import_PVGPpy.py
@@ -1,5 +1,3 @@
 import sys
-import os
-path = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, '%s/../PVGPpy/' % path)
+sys.path.insert(0, '/Users/bane/Documents/school/GPVR/PVGP/')
 import PVGPpy
