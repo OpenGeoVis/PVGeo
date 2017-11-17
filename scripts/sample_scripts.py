edd7b75af00753f01e893a9e10ef483650ffa02f
Bane Sullivan <chrsulli@mines.edu>
2017-11-17 15:05:33 -0700

    more imports (import our filters)

--- scripts/sample_scripts.py
+++ scripts/sample_scripts.py
@@ -1,8 +1,19 @@
 #! pvpython
 
-#### import the simple module from the paraview
+# import the simple module from the paraview
 from paraview.simple import *
+from os import listdir
+from os.path import isfile, join
 from PVGPpy import *
+
+# TODO: make sure this is the same as the PVPATH variable in the installation script
+PVPATH = '.'
+filters = [f for f in listdir(PVPATH) if f.endswith(".xml")]
+for f in filters:
+    LoadPlugin(PVPATH + f, remote=True, ns=globals())
+
+
+
 #### disable automatic camera reset on 'Show'
 paraview.simple._DisableFirstRenderCameraReset()
 
