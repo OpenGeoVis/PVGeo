bebf75f78ee7e8893a2de33fc94d356abd7fff66
Bane Sullivan <chrsulli@mines.edu>
2018-01-16 14:13:06 -0700

    added progress bar output to helpers submodule

---
+++ PVGPpy/helpers/progress.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+
+# Print iterations progress
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='#'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+
+    Example Use:
+    printProgressBar(i, len(arr), prefix = 'Progress:', suffix = 'Complete', length = 50)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end='\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
