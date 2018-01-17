43bc2b7b92b1856a2db4aecba6a57791f0bfad7c
Bane Sullivan <chrsulli@mines.edu>
2018-01-16 18:23:03 -0700

    All useful filters are now implemented in the PVGPpy module

--- PVGPpy/filt/tables.py
+++ PVGPpy/filt/tables.py
@@ -49,3 +49,56 @@ def reshapeTable(pdi, nrows, ncols, pdo=None):
         pdo.GetRowData().AddArray(insert) # NOTE: this is in the FieldData
 
     return pdo
+
+
+#---- LatLon to Cartesian ----#
+def latLonTableToCartesian(pdi, lat_i, lon_i, radius=6371.0, pdo=None):
+    # TODO: This is very poorly done
+    # TODO: filter works but assumes a spherical earth wich is very wrong
+    # NOTE: Msatches the vtkEarth SOurce however so we gonna keep it this way
+    if pdo is None:
+        pdo = vtk.vtkTable()
+    pdo.DeepCopy(pdi)
+
+    # Get number of columns
+    ncols = pdi.GetNumberOfColumns()
+    nrows = pdi.GetColumn(0).GetNumberOfTuples()
+
+    # Make a 2D numpy array and fille with data from input table
+    data = np.empty((nrows,ncols))
+    for i in range(ncols):
+        c = pdi.GetColumn(i)
+        data[:,i] = nps.vtk_to_numpy(c)
+
+    rad = 2 * np.pi / 360.0
+    coords = np.empty((nrows,3))
+    row_i = 0
+    '''
+    for r in data:
+        x = radius * cos(r[lat_i] * rad) * cos(r[lon_i] * rad)
+        y = radius * cos(r[lat_i] * rad) * sin(r[lon_i] * rad)
+        z = radius * sin(r[lat_i] * rad)
+        coords[row_i] = [x,y,z]
+        row_i = row_i + 1
+    '''
+    x = radius * cos(data[:,lat_i] * rad) * cos(data[:,lon_i] * rad)
+    y = radius * cos(data[:,lat_i] * rad) * sin(data[:,lon_i] * rad)
+    z = radius * sin(data[:,lat_i] * rad)
+    coords[:,0] = x
+    coords[:,1] = y
+    coords[:,2] = z
+    # Add coords to ouptut table
+    for i in range(3):
+        col = np.array(coords[:,i])
+        insert = nps.numpy_to_vtk(num_array=col, deep=True) # array_type=vtk.VTK_FLOAT
+        # VTK arrays need a name.
+        if i == 0:
+            insert.SetName('X')
+        elif i == 1:
+            insert.SetName('Y')
+        elif i == 2:
+            insert.SetName('Z')
+        #pdo.AddColumn(insert) # these are not getting added to the output table
+        # ... work around:
+        pdo.GetRowData().AddArray(insert) # NOTE: this is in the FieldData
+    return pdo
--- src/filters/filter_lat_lon_to_cartesian.py
+++ src/filters/filter_lat_lon_to_cartesian.py
@@ -19,51 +19,8 @@ Properties = dict(
 # NOTE: Msatches the vtkEarth SOurce however so we gonna keep it this way
 
 def RequestData():
-    import numpy as np
-    from vtk.util import numpy_support as nps
+    from PVGPpy.filt import latLonTableToCartesian
     pdi = self.GetInput()
     pdo = self.GetOutput()
 
-    pdo.DeepCopy(pdi)
-
-    # Get number of columns
-    ncols = pdi.GetNumberOfColumns()
-    nrows = pdi.GetColumn(0).GetNumberOfTuples()
-
-    # Make a 2D numpy array and fille with data from input table
-    data = np.empty((nrows,ncols))
-    for i in range(ncols):
-        c = pdi.GetColumn(i)
-        data[:,i] = nps.vtk_to_numpy(c)
-
-    rad = 2 * np.pi / 360.0
-    coords = np.empty((nrows,3))
-    row_i = 0
-    '''
-    for r in data:
-        x = Radius * cos(r[lat_i] * rad) * cos(r[lon_i] * rad)
-        y = Radius * cos(r[lat_i] * rad) * sin(r[lon_i] * rad)
-        z = Radius * sin(r[lat_i] * rad)
-        coords[row_i] = [x,y,z]
-        row_i = row_i + 1
-    '''
-    x = Radius * cos(data[:,lat_i] * rad) * cos(data[:,lon_i] * rad)
-    y = Radius * cos(data[:,lat_i] * rad) * sin(data[:,lon_i] * rad)
-    z = Radius * sin(data[:,lat_i] * rad)
-    coords[:,0] = x
-    coords[:,1] = y
-    coords[:,2] = z
-    # Add coords to ouptut table
-    for i in range(3):
-        col = np.array(coords[:,i])
-        insert = nps.numpy_to_vtk(num_array=col, deep=True) # array_type=vtk.VTK_FLOAT
-        # VTK arrays need a name.
-        if i == 0:
-            insert.SetName('X')
-        elif i == 1:
-            insert.SetName('Y')
-        elif i == 2:
-            insert.SetName('Z')
-        #pdo.AddColumn(insert) # these are not getting added to the output table
-        # ... work around:
-        pdo.GetRowData().AddArray(insert) # NOTE: this is in the FieldData
+    latLonTableToCartesian(pdi, lat_i, lon_i, radius=Radius, pdo=pdo)
