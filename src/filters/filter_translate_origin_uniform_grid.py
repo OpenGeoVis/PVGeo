616571cc353e157a4301e960ffa6d0378dce26e1
Bane Sullivan <chrsulli@mines.edu>
2018-01-16 17:13:15 -0700

    In process of transfering filters to the PVGPpy module.

--- src/filters/filter_translate_origin_uniform_grid.py
+++ src/filters/filter_translate_origin_uniform_grid.py
@@ -36,50 +36,8 @@ Properties = dict(
 
 
 def RequestData():
+    from PVGPpy.filt import translateGridOrigin
     pdi = self.GetInput() # vtkImageData
     pdo = self.GetOutput() # vtkImageData
 
-    [nx, ny, nz] = pdi.GetDimensions()
-    [ox, oy, oz] = pdi.GetOrigin()
-
-    pdo.DeepCopy(pdi)
-
-    xx,yy,zz = 0.0,0.0,0.0
-
-    if Corner == 1:
-        # South East Bottom
-        xx = ox - nx
-        yy = oy
-        zz = oz
-    elif Corner == 2:
-        # North West Bottom
-        xx = ox
-        yy = oy - ny
-        zz = oz
-    elif Corner == 3:
-        # North East Bottom
-        xx = ox - nx
-        yy = oy - ny
-        zz = oz
-    elif Corner == 4:
-        # South West Top
-        xx = ox
-        yy = oy
-        zz = oz - nz
-    elif Corner == 5:
-        # South East Top
-        xx = ox - nx
-        yy = oy
-        zz = oz - nz
-    elif Corner == 6:
-        # North West Top
-        xx = ox
-        yy = oy - ny
-        zz = oz - nz
-    elif Corner == 7:
-        # North East Top
-        xx = ox - nx
-        yy = oy - ny
-        zz = oz - nz
-
-    pdo.SetOrigin(xx, yy, zz)
+    translateGridOrigin(pdi, corner=Corner, pdo=pdo)
