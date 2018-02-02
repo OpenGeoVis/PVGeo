63efc364b965c4f7b3f7aa50eada2133253ee79b
Bane Sullivan <chrsulli@mines.edu>
2018-02-02 00:59:40 -0700

    removed broken server manager imports

--- pvmacros/__init__.py
+++ pvmacros/__init__.py
@@ -2,12 +2,3 @@ __all__ = [
     "export",
     "vis"
 ]
-
-from paraview.simple import LoadPlugin
-import os
-
-os.path.abspath(__file__)
-PVPATH = "%s/../plugins/" % os.path.abspath(__file__)
-filters = [f for f in os.listdir(PVPATH) if f.endswith(".xml")]
-for f in filters:
-    LoadPlugin(PVPATH + f, remote=True, ns=globals())
