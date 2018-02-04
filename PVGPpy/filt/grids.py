ec08f47da98fda77ca5bb921e4f37d62c1c73d86
Bane Sullivan <chrsulli@mines.edu>
2018-02-03 23:47:16 -0700

    Discarded attempted fix

--- PVGPpy/filt/grids.py
+++ PVGPpy/filt/grids.py
@@ -4,41 +4,78 @@ import numpy as np
 
 #---- Table To Grid Stuff ----#
 
-def _flip(arr, axis):
-    if axis == 2:
-        return np.flipud(arr)
-    if axis == 1:
-        return np.fliplr(arr)
-    if axis == 0:
-        arr = np.transpose(arr, (2,1,0))
-        arr = np.flipud(arr)
-        return np.transpose(arr, (2,1,0))
-
-def _refold(arr, extent, order='F', flip=[]):
+def _unpack(arr, extent, order='C'):
     """
     This is a helper method that handles the initial unpacking of a data array.
     ParaView and VTK use Fortran packing so this is convert data saved in
     C packing to Fortran packing.
-    the extent passed to this function is straight from the user...
-        either C (nx,ny,nz) or F (nz,ny,nz)
-    the extent returned is (nz,ny,nx)
     """
-    nx,ny,nz = extent[0],extent[1],extent[2]
-
+    n1,n2,n3 = extent[0],extent[1],extent[2]
     if order == 'C':
-        arr = np.reshape(arr, (nx,ny,nz))
-        arr = np.swapaxes(arr,0,2) # Shape should be (nz,ny,nx)
+        arr = np.reshape(arr, (n1,n2,n3))
+        arr = np.swapaxes(arr,0,2)
+        extent = np.shape(arr)
     elif order == 'F':
-        arr = np.reshape(arr, (nz,ny,nx))
+        # effectively doing nothing
+        #arr = np.reshape(arr, (n3,n2,n1))
+        return arr.flatten(), extent
+    return arr.flatten(), extent
 
-    # flip
-    for i in flip:
-        print('Flipping axis:', i)
-        arr = _flip(arr, i)
 
-    return arr.flatten(), (nz,ny,nx)
+def _rearangeSEPlib(arr, extent):
+    """
+    This is a helper method to swap axes   when using SEPlib axial conventions.
+    """
+    n1,n2,n3 = extent[0],extent[1],extent[2]
+    arr = np.reshape(arr, (n3,n2,n1))
+    arr = np.swapaxes(arr,0,2)
+    return arr.flatten(), (n1,n2,n3)
 
-def tableToGrid(pdi, extent, spacing=(1,1,1), origin=(0,0,0), order='F', flip=[], pdo=None):
+def _transposeXY(arr, extent, SEPlib=False):
+    """
+    Transposes X and Y axes. Needed by Whitney's research group for PoroTomo.
+    """
+    n1,n2,n3 = extent[0],extent[1],extent[2]
+    arr = np.reshape(arr, (n1,n2,n3))
+    if SEPlib:
+        arr = np.swapaxes(arr,1,2)
+        ext = (n1,n3,n2)
+    else:
+        print('bef: ', np.shape(arr))
+        arr = np.swapaxes(arr,0,1)
+        print('aft: ', np.shape(arr))
+        ext = np.shape(arr)
+    return arr.flatten(), ext
+
+
+def _refold(arr, extent, SEPlib=True, order='F', swapXY=False):
+    """
+    This is a helper method to handle grabbing a data array and make sure it is
+    ready for VTK/Fortran ordering in vtkImageData.
+    """
+    # Fold into 3D using extents. Packing dimensions should be in order extent
+    arr, extent = _unpack(arr, extent, order=order)
+    if SEPlib:
+        arr, extent = _rearangeSEPlib(arr, extent)
+    if swapXY:
+        arr, extent = _transposeXY(arr, extent, SEPlib=SEPlib)
+    return arr, extent
+
+def refoldidx(SEPlib=True, swapXY=False):
+    """
+    Theses are indexing corrections to set the spacings and origin witht the correct axes after refolding.
+    """
+    if SEPlib:
+        idx = (2,1,0)
+        if swapXY:
+            idx = (1,2,0)
+    else:
+        idx = (0,1,2)
+        if swapXY:
+            idx = (1,0,2)
+    return idx
+
+def tableToGrid(pdi, extent, spacing=(1,1,1), origin=(0,0,0), SEPlib=True, order='F', swapXY=False, pdo=None):
     """
     Converts a table of data arrays to vtkImageData given an extent to reshape that table.
     Each column in the table will be treated as seperate data arrays for the described data space
@@ -50,9 +87,10 @@ def tableToGrid(pdi, extent, spacing=(1,1,1), origin=(0,0,0), order='F', flip=[]
     if pdo is None:
         pdo = vtk.vtkImageData()
 
-    nx,ny,nz = extent[0],extent[1],extent[2]
-    sx,sy,sz = spacing[0],spacing[1],spacing[2]
-    ox,oy,oz = origin[0],origin[1],origin[2]
+    idx = refoldidx(SEPlib=SEPlib, swapXY=swapXY)
+    nx,ny,nz = extent[idx[0]],extent[idx[1]],extent[idx[2]]
+    sx,sy,sz = spacing[idx[0]],spacing[idx[1]],spacing[idx[2]]
+    ox,oy,oz = origin[idx[0]],origin[idx[1]],origin[idx[2]]
     # make sure dimensions work
     if (nx*ny*nz != rows):
         raise Exception('Total number of elements must remain %d. Check reshape dimensions (n1 by n2 by n3).' % (rows))
@@ -67,7 +105,7 @@ def tableToGrid(pdi, extent, spacing=(1,1,1), origin=(0,0,0), order='F', flip=[]
         c = pdi.GetColumn(i)
         name = c.GetName()
         arr = nps.vtk_to_numpy(c)
-        arr, ext = _refold(arr, extent, order=order, flip=flip)
+        arr, ext = _refold(arr, extent, SEPlib=SEPlib, order=order, swapXY=swapXY)
         c = nps.numpy_to_vtk(num_array=arr,deep=True)
         c.SetName(name)
         #pdo.GetCellData().AddArray(c) # Should we add here? flipper won't flip these...
@@ -140,7 +178,6 @@ def translateGridOrigin(pdi, corner=1, pdo=None):
 
     [nx, ny, nz] = pdi.GetDimensions()
     [ox, oy, oz] = pdi.GetOrigin()
-    # TODO: what about spacing???
 
     pdo.DeepCopy(pdi)
 
--- src/filters/filter_table_to_uniform_grid.py
+++ src/filters/filter_table_to_uniform_grid.py
@@ -1,6 +1,6 @@
 Name = 'TableToUniformGrid'
 Label = 'Table To Uniform Grid'
-FilterCategory = 'CSM GP Filters'
+FilterCategory = 'CSM Geophysics Filters'
 Help = 'This filter takes a vtkTable object with columns that represent data to be translated (reshaped) into a 3D grid (2D also works, just set the third dimensions extent to 1). The grid will be a n1 by n2 by n3 vtkImageData structure and an origin (south-west bottom corner) can be set at any xyz point. Each column of the vtkTable will represent a data attribute of the vtkImageData formed (essentially a uniform mesh). The SEPlib option allows you to unfold data that was packed in the SEPlib format where the most important dimension is z and thus the z data is d1 (d1=z, d2=y, d3=x). When using SEPlib, specify n1 as the number of elements in the Z-direction, n2 as the number of elements in the X-direction, and n3 as the number of elements in the Y-direction (and so on for other parameters).'
 
 NumberOfInputs = 1
@@ -14,8 +14,8 @@ ExtraXml = '''
     initial_string="test_drop_down_menu"
     default_values="0">
     <EnumerationDomain name="enum">
-            <Entry value="0" text="C-style: Row-major order"/>
-            <Entry value="1" text="Fortran-style: column-major order"/>
+            <Entry value="0" text="Fortran-style: column-major order"/>
+            <Entry value="1" text="C-style: Row-major order"/>
     </EnumerationDomain>
     <Documentation>
         This is the type of memory ordering to use.
@@ -29,10 +29,13 @@ Properties = dict(
     extent=[1, 1, 1],
     spacing=[1.0, 1.0, 1.0],
     origin=[0.0, 0.0, 0.0],
-    Flip_X=False,
-    Flip_Y=False,
-    Flip_Z=False,
+    SEPlib=True,
+    Transpose_XY=True,
     order=0
+)
+
+PropertiesHelp = dict(
+    SEPlib='Use the Stanford Exploration Project\'s axial conventions (d1=z, d2=x, d3=y). Parameters would be entered [z,x,y].'
 )
 
 def RequestData():
@@ -41,27 +44,24 @@ def RequestData():
     import numpy as np
 
     if order == 0:
-        mem = 'C'
-    elif order == 1:
         mem = 'F'
+    elif order == 1:
+        mem = 'C'
     else:
         mem = 'C'
-
-    flips = []
-    if Flip_X: flips.append(0)
-    if Flip_Y: flips.append(1)
-    if Flip_Z: flips.append(2)
 
     pdi = self.GetInput()
     image = self.GetOutput() #vtkImageData
 
-    tableToGrid(pdi, extent, spacing, origin, order=mem, flip=flips, pdo=image)
+    tableToGrid(pdi, extent, spacing, origin, SEPlib=SEPlib, order=mem, swapXY=Transpose_XY, pdo=image)
 
 
 
 def RequestInformation():
     from paraview import util
+    from PVGPpy.filt import refoldidx
     # Setup the ImageData
-    nx,ny,nz = extent[0],extent[1],extent[2]
+    idx = refoldidx(SEPlib=SEPlib, swapXY=Transpose_XY)
+    nx,ny,nz = extent[idx[0]],extent[idx[1]],extent[idx[2]]
     # ABSOLUTELY NECESSARY FOR THE FILTER TO WORK:
     util.SetOutputWholeExtent(self, [0,nx-1, 0,ny-1, 0,nz-1])
