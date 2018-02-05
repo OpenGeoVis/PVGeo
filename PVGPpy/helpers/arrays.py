610df63980756018d81a6323903e490c5ce4f24b
Bane Sullivan <chrsulli@mines.edu>
2018-02-04 19:27:12 -0700

    helper to moved arrays to point data

--- PVGPpy/helpers/arrays.py
+++ PVGPpy/helpers/arrays.py
@@ -8,6 +8,35 @@ def getSelectedArrayField(algorithm, idx):
     info = algorithm.GetInputArrayInformation(idx)
     return info.Get(vtk.vtkDataObject.FIELD_ASSOCIATION())
 
+
+def copyArraysToPointData(pdi, pdo, field):
+    # Point Data
+    if field == 0:
+        for i in range(pdi.GetPointData().GetNumberOfArrays()):
+            arr = pdi.GetPointData().GetArray(i)
+            pdo.GetPointData().AddArray(arr)
+    # Cell Data: DO NOT USE
+    elif field == 1:
+        for i in range(pdi.GetCellData().GetNumberOfArrays()):
+            arr = pdi.GetCellData().GetArray(i)
+            pdo.GetPointData().AddArray(arr)
+    # Field Data:
+    elif field == 2:
+        for i in range(pdi.GetFieldData().GetNumberOfArrays()):
+            arr = pdi.GetFieldData().GetArray(i)
+            pdo.GetPointData().AddArray(arr)
+    # Row Data:
+    elif field == 6:
+        for i in range(pdi.GetRowData().GetNumberOfArrays()):
+            arr = pdi.GetRowData().GetArray(i)
+            pdo.GetPointData().AddArray(arr)
+    else:
+        raise Exception('Field association not defined. Try inputing Point, Cell, Field, or Row data.')
+    # Point Data
+
+    # Field Data
+    return pdo
+
 def getArray(wpdi, field, name):
     """
     Grabs an array from vtkDataObject given its name and field association
