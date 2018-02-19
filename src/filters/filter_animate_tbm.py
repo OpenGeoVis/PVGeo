96a4b0095d7e4b1b47b245e319c1184683c3afda
Bane Sullivan <chrsulli@mines.edu>
2018-02-18 20:55:23 -0700

    update animate TBM

--- src/filters/filter_animate_tbm.py
+++ src/filters/filter_animate_tbm.py
@@ -67,31 +67,24 @@ def RequestData():
         # TODO: if we want to account for length of actual parts of the machine
         # then calc extra points here and append to pts in correct order
         pts.append((x,y,z))
+        # TODO: if we want to add data attributes append them here
     # now exectute a points to tube filter
     vtk_pts = vtk.vtkPoints()
     for i in range(len(pts)):
         vtk_pts.InsertNextPoint(pts[i][0],pts[i][1],pts[i][2])
-    # missing something here
     poly = vtk.vtkPolyData()
     poly.SetPoints(vtk_pts)
     pointsToTube(poly, radius=Diameter/2, numSides=20, nrNbr=False, pdo=pdo)
 
 
 def RequestInformation(self):
-    # This is necessary to set time steps
+    import numpy as np
     executive = self.GetExecutive()
     outInfo = executive.GetOutputInformation(0)
-
     # Calculate list of timesteps here
-    # set up time space
-    def _calculateTimeRange(num, dt=0.001):
-        """Discretizes time range accoridng to step size `dt` in seconds"""
-        import numpy as np
-        return np.arange(0,num*dt,dt, dtype=float)
     #- Get number of rows in table and use that for num time steps
-    pdi = self.GetInput()
-    nrows = int(pdi.GetColumn(0).GetNumberOfTuples())
-    xtime = _calculateTimeRange(nrows,dt=dt)
+    nrows = int(self.GetInput().GetColumn(0).GetNumberOfTuples())
+    xtime = np.arange(0,nrows*dt,dt, dtype=float)
     outInfo.Remove(executive.TIME_STEPS())
     for i in range(len(xtime)):
         outInfo.Append(executive.TIME_STEPS(), xtime[i])
