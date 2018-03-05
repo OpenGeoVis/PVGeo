bd95d2a45997b08e2ec433b466e8dd370208ba68
Bane Sullivan <chrsulli@mines.edu>
2018-03-04 22:26:28 -0700

    Added macro to use custom axis ticks

--- pvmacros/vis/__init__.py
+++ pvmacros/vis/__init__.py
@@ -1,3 +1,4 @@
 from manySlicesAlongPoints import *
 from clipThrough import *
 from camera import *
+from axes import *
---
+++ pvmacros/vis/axes.py
@@ -0,0 +1,16 @@
+from paraview.simple import GetActiveViewOrCreate, RenderAllViews
+
+def customAxisTicks(rng, axis=0, uniform=False):
+    # note that third parameter is the step size
+    # get the active view
+    rv = GetActiveViewOrCreate('RenderView')
+    if axis is 0 or uniform:
+        rv.AxesGrid.XAxisUseCustomLabels = 1
+        rv.AxesGrid.XAxisLabels = rng
+    if axis is 1 or uniform:
+        rv.AxesGrid.YAxisUseCustomLabels = 1
+        rv.AxesGrid.YAxisLabels = rng
+    if axis is 2 or uniform:
+        rv.AxesGrid.ZAxisUseCustomLabels = 1
+        rv.AxesGrid.ZAxisLabels = rng
+    RenderAllViews()
