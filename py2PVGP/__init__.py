8583e65cf816f24caf3f56cbf6ee14c1dfb79311
Bane Sullivan <chrsulli@mines.edu>
2018-03-04 22:36:49 -0700

    added description

--- py2PVGP/__init__.py
+++ py2PVGP/__init__.py
@@ -1,3 +1,6 @@
+"""
+This module is meant to be used outside of pvpython in python 2 or 3. This has functionality that might be useful for transfering data from a python working space to a ParaView working space.
+"""
 import os
 import json
 import struct
