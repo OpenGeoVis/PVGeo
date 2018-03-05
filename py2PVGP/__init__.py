0ccd2d04ccf5f4dbb3f94e2d0a2815a508685e2f
Bane Sullivan <chrsulli@mines.edu>
2018-03-05 11:27:52 -0700

    expanding py2PVGP module

--- py2PVGP/__init__.py
+++ py2PVGP/__init__.py
@@ -1,10 +1,7 @@
 """
 This module is meant to be used outside of pvpython in python 2 or 3. This has functionality that might be useful for transfering data from a python working space to a ParaView working space.
 """
-import os
-import json
-import struct
-import numpy as np
+from file_sinks import *
 from matplotlib.colors import LinearSegmentedColormap
 # This is so we can use the Parula Colormap in matplotlib
 cm_data = [[0.2081, 0.1663, 0.5292], [0.2116238095, 0.1897809524, 0.5776761905],
@@ -51,77 +48,3 @@ cm_data = [[0.2081, 0.1663, 0.5292], [0.2116238095, 0.1897809524, 0.5776761905],
 
 def getParulaMap():
     return LinearSegmentedColormap.from_list('parula', cm_data)
-
-
-def _getdtypes(dtype):
-    if dtype == 'float64':
-        num_bytes = 8 # DOUBLE
-        sdtype = 'd'
-    elif dtype == 'float32':
-        num_bytes = 4 # FLOAT
-        sdtype = 'f'
-    elif 'int' in dtype:
-        num_bytes = 4 # INTEGER
-        sdtype = 'i'
-    else:
-        raise Exception('dtype \'%s\' unknown.' % dtype)
-
-    return dtype, sdtype, num_bytes
-
-
-def savePVGPGrid(data, path, basename, spacing=(1,1,1), origin=(0,0,0), order='F', dataNames=None, endian='@'):
-    if type(data) is not list:
-        data = [data]
-    numArrays = len(data)
-    if dataNames is not None and len(dataNames) != numArrays:
-        raise Exception('%d data array names needed. %d given to `dataNames`.' % (numArrays, len(dataNames)))
-    elif dataNames is None:
-        dataNames = []
-        for i in range(numArrays):
-            dataNames.append('Array%0d' % i)
-
-
-
-    shps = []
-    dtypes = []
-    for arr in data:
-        shps.append(np.shape(arr))
-        dtypes.append(str(arr.dtype))
-    # TODO: check that all data array shapes are the same
-    ext = shps[0]
-
-    # save the data arrays
-    dataArrsDict = dict()
-    for i in range(numArrays):
-        # save out each data array to own file
-        #format is `basename-%d.pvgp@`
-        dd = data[i].flatten(order=order)
-        no, sdtype, num_bytes = _getdtypes(str(dd.dtype))
-        dd = struct.pack(endian+str(len(dd))+sdtype,*dd)
-
-        fname = '%s-%s.pvgp@' % (basename,dataNames[i])
-        dataArrsDict[dataNames[i]] = dict(
-            filemane=fname,
-            dtype=dtypes[i]
-        )
-        with open('%s/%s' % (path, fname), 'wb') as f:
-            f.write(dd)
-
-    # Parse out the header
-    lib = dict(
-        basename=basename,
-        extent=ext,
-        spacing=spacing,
-        origin=origin,
-        order=order,
-        endian=endian,
-        numArrays=numArrays,
-        dataArrays=dataArrsDict,
-        originalPath=path
-    )
-
-    # save the header
-    with open('%s/%s.pvgp' % (path, basename), 'w') as fp:
-        json.dump(lib, fp, indent=4)
-
-    return None
---
+++ py2PVGP/file_sinks.py
@@ -0,0 +1,77 @@
+import os
+import json
+import struct
+import numpy as np
+
+def _getdtypes(dtype):
+    if dtype == 'float64':
+        num_bytes = 8 # DOUBLE
+        sdtype = 'd'
+    elif dtype == 'float32':
+        num_bytes = 4 # FLOAT
+        sdtype = 'f'
+    elif 'int' in dtype:
+        num_bytes = 4 # INTEGER
+        sdtype = 'i'
+    else:
+        raise Exception('dtype \'%s\' unknown.' % dtype)
+
+    return dtype, sdtype, num_bytes
+
+
+def savePVGPGrid(data, path, basename, spacing=(1,1,1), origin=(0,0,0), order='F', dataNames=None, endian='@'):
+    if type(data) is not list:
+        data = [data]
+    numArrays = len(data)
+    if dataNames is not None and len(dataNames) != numArrays:
+        raise Exception('%d data array names needed. %d given to `dataNames`.' % (numArrays, len(dataNames)))
+    elif dataNames is None:
+        dataNames = []
+        for i in range(numArrays):
+            dataNames.append('Array%0d' % i)
+
+
+
+    shps = []
+    dtypes = []
+    for arr in data:
+        shps.append(np.shape(arr))
+        dtypes.append(str(arr.dtype))
+    # TODO: check that all data array shapes are the same
+    ext = shps[0]
+
+    # save the data arrays
+    dataArrsDict = dict()
+    for i in range(numArrays):
+        # save out each data array to own file
+        #format is `basename-%d.pvgp@`
+        dd = data[i].flatten(order=order)
+        no, sdtype, num_bytes = _getdtypes(str(dd.dtype))
+        dd = struct.pack(endian+str(len(dd))+sdtype,*dd)
+
+        fname = '%s-%s.pvgp@' % (basename,dataNames[i])
+        dataArrsDict[dataNames[i]] = dict(
+            filemane=fname,
+            dtype=dtypes[i]
+        )
+        with open('%s/%s' % (path, fname), 'wb') as f:
+            f.write(dd)
+
+    # Parse out the header
+    lib = dict(
+        basename=basename,
+        extent=ext,
+        spacing=spacing,
+        origin=origin,
+        order=order,
+        endian=endian,
+        numArrays=numArrays,
+        dataArrays=dataArrsDict,
+        originalPath=path
+    )
+
+    # save the header
+    with open('%s/%s.pvgp' % (path, basename), 'w') as fp:
+        json.dump(lib, fp, indent=4)
+
+    return None
