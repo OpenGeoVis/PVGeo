8848ce4b4d7d65547e0273e2644faecbefa1bb53
Bane Sullivan <chrsulli@mines.edu>
2018-05-26 01:18:25 -0600

    The UBD Add Model to Mesh filter now handles 2D and 3D

--- src/filters/filter_ubc_add_model.py
+++ src/filters/filter_ubc_add_model.py
@@ -1,9 +1,9 @@
-Name = 'AddUBCModelToMesh3D'
-Label = 'Add UBC Model To Mesh 3D'
+Name = 'AddUBCModelToMesh'
+Label = 'Add UBC Model To Mesh'
 FilterCategory = 'PVGP Filters'
 
 # A general overview of the plugin
-Help = 'This filter allows you to choose a UBC model file to append as an attribute to an already created vtkRectilinearGrid of a UBC 3D Mesh.'
+Help = 'This filter allows you to choose a UBC model file to append as an attribute to an already created vtkRectilinearGrid of a UBC 2D or 3D Mesh.'
 
 NumberOfInputs = 1
 InputDataType = 'vtkRectilinearGrid'
@@ -14,12 +14,13 @@ ExtraXml = ''
 
 # These are the parameters/properties of the plugin:
 Properties = dict(
+    _2D=False,
     FileName_Model='absolute path',
     Data_Name=''
 )
 
 def RequestData():
-    from PVGPpy.read import ubcModel3D, placeModelOnMesh
+    from PVGPpy.read import ubcModel3D, ubcModel2D, placeModelOnMesh
     import os
 
     if FileName_Model == 'absolute path':
@@ -32,5 +33,7 @@ def RequestData():
 
     pdo.DeepCopy(pdi) # ShallowCopy if you want changes to propagate
 
-    model = ubcModel3D(FileName_Model)
+    if _2D: model = ubcModel2D(FileName_Model)
+    else: model = ubcModel3D(FileName_Model)
+    # Place read model on the mesh
     placeModelOnMesh(pdo, model, dataNm=Data_Name)
