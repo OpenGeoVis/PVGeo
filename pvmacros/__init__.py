6dbf80d8f9d6afd025b0643b925894d85885fe81
Bane Sullivan <chrsulli@mines.edu>
2018-05-31 14:11:13 -0600

    Implemented deleteDownstream() in pvmacros

--- pvmacros/__init__.py
+++ pvmacros/__init__.py
@@ -8,3 +8,27 @@ from . import vis
 __author__ = 'Bane Sullivan'
 __license__ = 'BSD-3-Clause'
 __copyright__ = '2018, Bane Sullivan'
+
+
+def deleteDownstream(input=None):
+    import paraview.simple as pvs
+    if input is None:
+        # The below snippet deletes all Filters on the pipeline
+        #- i.e. deletes anything that has an input
+        #- preserves readers and sources
+        for f in pvs.GetSources().values():
+            if f.GetProperty("Input") is not None:
+                pvs.Delete(f)
+    else:
+        # Be able to specify upstream source
+        src = pvs.FindSource(input)
+        #print('src: ', src)
+        # Delete ALL things downstream of input
+        for f in pvs.GetSources().values():
+            #print('f: ', f)
+            #print('f.Input: ', f.GetProperty("Input"))
+            if f.GetPropertyValue("Input") is src:
+                #print('Deleting: ', f)
+                pvs.Delete(f)
+    # Done
+    return None
--- pvmacros/vis/__init__.py
+++ pvmacros/vis/__init__.py
@@ -3,16 +3,6 @@ from .objs import *
 from .axes import *
 from .slices import *
 
-def deleteFilters(input=None):
-    import paraview.simple as pvs
-    """if input is not None:
-        src = pvs.FindSource(dataNm)"""
-    #TODO: be able to specify upstream source
-    for f in pvs.GetSources().values():
-        if f.GetProperty("Input") is not None:
-            pvs.Delete(f)
-    return None
-
 def hideAll():
     """This hides all sources/filters on the pipeline from the current view"""
     import paraview.simple as pvs
