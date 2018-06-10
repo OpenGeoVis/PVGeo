8a55d2255f1157dc100ce8f932b75d31bb321950
Bane Sullivan <chrsulli@mines.edu>
2018-06-10 14:54:03 -0600

    Added pvpluigns template to each submodule

---
+++ PVGPpy/ubc/pvplugins.py
@@ -0,0 +1,11 @@
+"""
+This file contains the necessary VTKPythonAlgorithmBase subclasses to implement
+functionality in this submodule as filters, sources, readers, and writers in
+ParaView.
+"""
+
+
+# This is module to import. It provides VTKPythonAlgorithmBase, the base class
+# for all python-based vtkAlgorithm subclasses in VTK and decorators used to
+# 'register' the algorithm with ParaView along with information about UI.
+from paraview.util.vtkAlgorithm import *
