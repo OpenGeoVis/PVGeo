671b58ddbcac8ebebe225698824d9a70a5be769c
Bane Sullivan <chrsulli@mines.edu>
2018-06-10 15:15:04 -0600

    removed delimited text reader

--- PVGPpy/readers_general/binaries.py
+++ PVGPpy/readers_general/binaries.py
@@ -1,7 +1,6 @@
 __all__ = [
     'packedBinaries',
-    'madagascar',
-    'delimitedText']
+    'madagascar']
 
 import numpy as np
 import csv
@@ -87,50 +86,5 @@ def madagascar(FileName, dataNm=None, endian=None, dtype='f', pdo=None):
 
     # Table with single column of data only
     pdo.AddColumn(data)
-
-    return pdo
-
-
-def delimitedText(FileName, deli=' ', useTab=False, hasTits=True, skiprows=0, comments='#', pdo=None):
-    """
-    @desc:
-    This reader will take in any delimited text file and make a vtkTable from it. This is not much different than the default .txt or .csv reader in ParaView, however it gives us room to use our own extensions and a little more flexibility in the structure of the files we import.
-
-
-    @params:
-    FileName : str: req : The absolute file name with path to read.
-    deli : str : opt : The input files delimiter. To use a tab delimiter please set the `useTab`.
-    useTab : boolean : opt : A boolean that describes whether to use a tab delimiter
-    hasTits : boolean : opt : A boolean for if the delimited file has header titles for the data arrays.
-    skiprows : int : opt : The integer number of rows to skip at the top of the file
-    comments : char : opt : The identifier for comments within the file.
-    pdo : vtk.vtkTable : opt : A pointer to the output data object.
-
-    @return:
-    vtkTable : Returns a vtkTable of the input data file.
-
-    """
-    if pdo is None:
-        pdo = vtk.vtkTable() # vtkTable
-
-    if (useTab):
-        deli = '\t'
-
-    fileLines = np.genfromtxt(FileName, dtype=str, delimiter='\n', comments=comments)
-
-    idx = 0
-    if hasTits:
-        titles = fileLines[idx+skiprows].split(deli)
-        idx += 1
-    else:
-        titles = []
-
-    data = np.genfromtxt((line.encode('utf8') for line in fileLines[idx+skiprows::]), dtype=None)
-
-    if not hasTits:
-        cols = np.shape(data)[1]
-        for i in range(cols):titles.append('Field %d' % i)
-
-    _placeArrInTable(data, titles, pdo)
 
     return pdo
