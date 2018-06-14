84e288f9f48a08dd927f410e0896dbfc931cabfb
Bane Sullivan <chrsulli@mines.edu>
2018-06-14 08:41:08 -0600

    fix typo

--- PVGeo/pvplugins/grids.py
+++ PVGeo/pvplugins/grids.py
@@ -8,7 +8,7 @@ ParaView.
 # This is module to import. It provides VTKPythonAlgorithmBase, the base class
 # for all python-based vtkAlgorithm subclasses in VTK and decorators used to
 # 'register' the algorithm with ParaView along with information about UI.
-f#TODO:from paraview.util.vtkAlgorithm import *
+#TODO:from paraview.util.vtkAlgorithm import *
 
 import numpy as np
 import vtk
