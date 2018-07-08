19b5d16a11c81d8c0af039eb26909db456ce0f4e
Bane Sullivan <banesullivan@gmail.com>
2018-07-07 19:08:44 -0600

    file management updates

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -5,3 +5,9 @@ from .model_build import *
 from .readers_general import *
 from .tunneling import *
 from .ubc import *
+
+
+__author__ = 'Bane Sullivan'
+__license__ = 'BSD-3-Clause'
+__copyright__ = '2018, Bane Sullivan'
+__version__ = '0.7.4'
