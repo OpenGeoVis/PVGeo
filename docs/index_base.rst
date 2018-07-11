59000c930fedfbf5e8d26829132a70b5bcb40fa1
Bane Sullivan <banesullivan@gmail.com>
2018-07-11 03:48:39 -0600

    Autodoc generator added

---
+++ docs/index_base.rst
@@ -0,0 +1,11 @@
+.. PVGeo documentation master file, created by
+   sphinx-quickstart on Tue Jul 10 19:56:04 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to PVGeo's code docs!
+=================================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Contents:
---
+++ docs/source/make_files.py
@@ -0,0 +1,64 @@
+import PVGeo
+import inspect
+import re
+import numpy as np
+
+
+##########
+
+classdoccer = r'''
+
+%s
+%s
+
+.. auto%s:: %s
+    :show-inheritance:
+    :members:
+    :undoc-members:
+
+'''
+
+defdoccer = r'''
+
+%s
+%s
+
+.. auto%s:: %s
+
+'''
+
+with open('../index_base.rst', 'r') as fid:
+    index = fid.read()
+    fid.close()
+
+
+############
+mods = inspect.getmembers(PVGeo, inspect.ismodule)
+
+modnames = []
+
+# Write out all of the pages
+for mod in mods:
+    if mod[0][0:2] == '__':
+        continue
+    name = mod[1].__displayname__
+    #print('On: ', name)
+    feats = inspect.getmembers(mod[1])
+    fname = name.replace(' ', '-')+'.rst'
+    index += '\n   suites/%s' % fname
+
+    with open('./suites/%s' % fname, 'w') as fid:
+        fid.write('%s\n%s' % (name, '='*len(name))) # Page header is mosule name
+        for f in feats:
+            if inspect.isclass(f[1]):
+                #print('\t' + f[1].__name__)
+                fid.write(classdoccer % (f[1].__name__, '-'*len(f[1].__name__), 'class', '%s.%s' % (mod[1].__name__, f[1].__name__) ))
+            elif inspect.isfunction(f[1]):
+                 fid.write(defdoccer % (f[1].__name__, '-'*len(f[1].__name__), 'function', '%s.%s' % (mod[1].__name__, f[1].__name__) ))
+
+
+        fid.close()
+
+
+with open('./index.rst', 'w') as fid:
+    fid.write(index)
