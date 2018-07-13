89a813eb4510c9fd6a18edc0a4521a5e1bbc7c65
Bane Sullivan <banesullivan@gmail.com>
2018-07-13 11:18:10 -0600

    Bump version: 1.1.2 → 1.1.3

--- pvmacros/__init__.py
+++ pvmacros/__init__.py
@@ -5,7 +5,7 @@ from .pipeline import *
 __author__ = 'Bane Sullivan'
 __license__ = 'BSD-3-Clause'
 __copyright__ = '2018, Bane Sullivan'
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 
 
 __displayname__ = 'ParaView Macros'
