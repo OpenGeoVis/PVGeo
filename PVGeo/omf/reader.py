c84e25e22b0c7ccc997ed4e909bfbf43fe4e338c
Bane Sullivan <banesullivan@gmail.com>
2018-07-17 14:26:56 -0600

    Update OMF reader to only read required data and only read it once

--- PVGeo/omf/reader.py
+++ PVGeo/omf/reader.py
@@ -28,6 +28,7 @@ class OMFReader(ReaderBaseBase):
         self._dataselection.AddObserver("ModifiedEvent", _helpers.createModifiedCallback(self))
         self.__names = []
         self.__data = dict()
+        self.__project = None
 
     def Modified(self, readAgain=False):
         """Ensure default is overridden to be false so array selector can call.
@@ -47,17 +48,23 @@ class OMFReader(ReaderBaseBase):
     def _ReadUpFront(self):
         # Read all elements
         reader = omf.OMFReader(self.GetFileName())
-        project = reader.get_project()
-        self.__names = [e.name for e in project.elements]
+        self.__project = reader.get_project()
+        self.__names = [e.name for e in self.__project.elements]
         for n in self.__names:
             self._dataselection.AddArray(n)
-
-        # Now iterate over the elements and add converted data to the data dict:
-        for e in project.elements:
-            d = e.toVTK()
-            self.__data[e.name] = d
         self.NeedToRead(flag=False)
         return 1
+
+    def _GetRawData(self):
+        """Converts OMF data to VTK data objects."""
+        # Now iterate over the elements and add converted data to the data dict:
+        data = dict()
+        for e in self.__project.elements:
+            if self._dataselection.ArrayIsEnabled(e.name):
+                if not e.name in self.__data:
+                    self.__data[e.name] = e.toVTK()
+                data[e.name] = self.__data[e.name]
+        return data
 
     #### pipeline methods ####
 
@@ -70,16 +77,16 @@ class OMFReader(ReaderBaseBase):
         # Perfrom the read
         if self.NeedToRead():
             self._ReadUpFront()
-
+        data = self._GetRawData()
         # Set number of blocks based on user choice in the selction
         output.SetNumberOfBlocks(self._dataselection.GetNumberOfArraysEnabled())
         blk = 0
         # iterate over data set to produce output based on users selection
-        for name in self.__names:
-            if self._dataselection.ArrayIsEnabled(name):
-                output.SetBlock(blk, self.__data[name])
-                output.GetMetaData(blk).Set(vtk.vtkCompositeDataSet.NAME(), name)
-                blk += 1
+        keys = data.keys()
+        for name in keys:
+            output.SetBlock(blk, data[name])
+            output.GetMetaData(blk).Set(vtk.vtkCompositeDataSet.NAME(), name)
+            blk += 1
         return 1
 
 
