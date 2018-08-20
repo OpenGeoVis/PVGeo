3b8396d55c806a39980e2df1279269c570ae8843
Bane Sullivan <banesullivan@gmail.com>
2018-08-19 19:17:16 -0600

    Bump version: 1.1.17 → 1.1.18

--- pvmacros/__init__.py
+++ pvmacros/__init__.py
@@ -5,7 +5,7 @@ from .pipeline import *
 __author__ = 'Bane Sullivan'
 __license__ = 'BSD-3-Clause'
 __copyright__ = '2018, Bane Sullivan'
-__version__ = '1.1.17'
+__version__ = '1.1.18'
 
 
 __displayname__ = 'ParaView Macros'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ and ParaView.
 
 import setuptools
 
-__version__ = '1.1.17'
+__version__ = '1.1.18'
 
 with open("README.md", "r") as f:
     long_description = f.read()
