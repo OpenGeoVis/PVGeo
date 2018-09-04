31580a7639012459bb8a5a8b9fd74cf836eb344b
Bane Sullivan <banesullivan@gmail.com>
2018-09-03 18:50:27 -0600

    Bump version: 1.1.21 → 1.1.22

--- PVGeo/__init__.py
+++ PVGeo/__init__.py
@@ -16,7 +16,7 @@ from . import version
 __author__ = 'Bane Sullivan'
 __license__ = 'BSD-3-Clause'
 __copyright__ = '2018, Bane Sullivan'
-__version__ = '1.1.21'
+__version__ = '1.1.22'
 __displayname__ = 'PVGeo'
 
 
