c231846b4245b5a4af39356505b47f702ee153ba
Bane Sullivan <banesullivan@gmail.com>
2018-09-25 15:44:24 -0600

    Remove pyproj dependency for windows

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
         'codecov==2.0.15',
         'pandas>=0.23.4',
         'mock>=2.0.0',
-        'pyproj>=1.9',
+        #'pyproj>=1.9',
     ],
     classifiers=(
         "Programming Language :: Python",
