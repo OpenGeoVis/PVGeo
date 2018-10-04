bae499a719be3a439ff62915d135850ab4b84bf1
Bane Sullivan <banesullivan@gmail.com>
2018-10-04 13:50:16 -0600

    Bump version: 1.1.30 → 1.1.31

--- PVGeo/__init__.py
+++ PVGeo/__init__.py
@@ -37,7 +37,7 @@ from . import version
 __author__ = 'Bane Sullivan'
 __license__ = 'BSD-3-Clause'
 __copyright__ = '2018, Bane Sullivan'
-__version__ = '1.1.30'
+__version__ = '1.1.31'
 __displayname__ = 'PVGeo'
 
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ and ParaView.
 
 import setuptools
 
-__version__ = '1.1.30'
+__version__ = '1.1.31'
 
 with open("README.md", "r") as f:
     long_description = f.read()
