0823e0d164993fa30e1e44e283ae4f10b7d085bd
Bane Sullivan <banesullivan@gmail.com>
2019-01-16 00:43:51 -0700

    Bump omfvtk

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setuptools.setup(
     install_requires=install_requires,
     extras_require={
         'pyproj': ['pyproj>=1.9'],
-        'omf': ['omf>=0.9.3', 'omfvtk>=0.0.2'],
+        'omf': ['omf>=0.9.3', 'omfvtk>=0.0.3'],
         'discretize': ['discretize>=0.3.8'],
     },
     classifiers=(
