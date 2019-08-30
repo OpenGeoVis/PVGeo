493091839ba70f395f9c8322b3dabe09245b3331
banesullivan <banesulli@gmail.com>
2019-08-30 10:14:34 -0600

    Update examples

--- examples/filters-general/extract-array.py
+++ examples/filters-general/extract-array.py
@@ -2,20 +2,26 @@
 Extract Array to Table
 ~~~~~~~~~~~~~~~~~~~~~~
 This example will demonstrate how to extract an array from any input data set
-to make a `vtkTable` of that single data array
+to make a :class:`pyvista.Table` of that single data array. Aftwards, we plot
+a histogram of that data array.
 
 This example demos :class:`PVGeo.filters.ExtractArray`
 """
 from PVGeo.filters import ExtractArray
 from pyvista import examples
+import matplotlib.pyplot as plt
+
 ###############################################################################
 # Create input data
-grd = examples.load_rectilinear()
+dataset = examples.download_st_helens()
 
 ###############################################################################
 # Construct the filter
 filt = ExtractArray()
 # Define the array to extract
 # Apply the filter on the input
-table = filt.apply(grd, 'Random Data')
+table = filt.apply(dataset, 'Elevation')
 print(table)
+
+###############################################################################
+plt.hist(table['Elevation'])
