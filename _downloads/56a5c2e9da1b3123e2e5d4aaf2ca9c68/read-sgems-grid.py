f97de152c53bdbdb3409e7c1ff9a1d0f022531f4
banesullivan <banesulli@gmail.com>
2019-10-27 19:17:42 +0000

    Update docs after building Travis build 848 of OpenGeoVis/PVGeo
    The docs were built from the branch 'master' against the commit
    69db227bb8b75634227cadb1258e127d22b6cf48.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/OpenGeoVis/PVGeo/jobs/603596252.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs ./docs/build/html .

--- _downloads/56a5c2e9da1b3123e2e5d4aaf2ca9c68/read-sgems-grid.py
+++ _downloads/56a5c2e9da1b3123e2e5d4aaf2ca9c68/read-sgems-grid.py
@@ -9,8 +9,9 @@ from pyvista import examples
 from PVGeo.gslib import SGeMSGridReader
 
 ###############################################################################
-grid_url = 'http://www.trainingimages.org/uploads/3/4/7/0/34703305/a_wlreferencecat.zip'
-filename, _ = examples.downloads._retrieve_file(grid_url, 'A_WLreferenceCAT.sgems.zip')
+
+# grid_url = 'http://www.trainingimages.org/uploads/3/4/7/0/34703305/a_wlreferencecat.zip'
+filename, _ = examples.downloads._download_file('A_WLreferenceCAT.sgems')
 
 grid = SGeMSGridReader().apply(filename)
 print(grid)
@@ -20,24 +21,27 @@ warped = grid.cell_data_to_point_data().warp_by_scalar(scale_factor=5)
 warped.plot()
 
 ###############################################################################
-grid_url = 'http://www.trainingimages.org/uploads/3/4/7/0/34703305/maules_creek_3d.zip'
-filename, _ = examples.downloads._retrieve_file(grid_url, 'Maules_Creek_3D.SGEMS.zip')
+
+# grid_url = 'http://www.trainingimages.org/uploads/3/4/7/0/34703305/maules_creek_3d.zip'
+filename, _ = examples.downloads._download_file('Maules_Creek_3D.SGEMS.zip')
 
 grid = SGeMSGridReader().apply(filename)
 grid.plot(categories=True)
 
 
 ###############################################################################
-grid_url = 'http://www.trainingimages.org/uploads/3/4/7/0/34703305/ti_horizons_continuous.zip'
-filename, _ = examples.downloads._retrieve_file(grid_url, 'TI_horizons_continuous.SGEMS.zip')
+
+# grid_url = 'http://www.trainingimages.org/uploads/3/4/7/0/34703305/ti_horizons_continuous.zip'
+filename, _ = examples.downloads._download_file('TI_horizons_continuous.SGEMS.zip')
 
 grid = SGeMSGridReader().apply(filename)
 grid.threshold([-4, 1.06]).plot(clim=grid.get_data_range())
 
 
 ###############################################################################
-grid_url = 'http://www.trainingimages.org/uploads/3/4/7/0/34703305/ti.zip'
-filename, _ = examples.downloads._retrieve_file(grid_url, 'ti.sgems.zip')
+
+# grid_url = 'http://www.trainingimages.org/uploads/3/4/7/0/34703305/ti.zip'
+filename, _ = examples.downloads._download_file('ti.sgems.zip')
 
 grid = SGeMSGridReader().apply(filename)
 grid.plot(scalars='photo', cpos='xy', cmap='bone')
--- _downloads/72449efec2368b5a0221d3a668dba2df/read-point-set.py
+++ _downloads/72449efec2368b5a0221d3a668dba2df/read-point-set.py
@@ -9,8 +9,9 @@ from pyvista import examples
 from PVGeo.gslib import GSLibPointSetReader
 
 ###############################################################################
-points_url = 'http://www.trainingimages.org/uploads/3/4/7/0/34703305/b_100sampledatawl.sgems'
-filename, _ = examples.downloads._retrieve_file(points_url, 'b_100sampledatawl.sgems')
+
+# points_url = 'http://www.trainingimages.org/uploads/3/4/7/0/34703305/b_100sampledatawl.sgems'
+filename, _ = examples.downloads._download_file('b_100sampledatawl.sgems')
 
 point_set = GSLibPointSetReader().apply(filename)
 print(point_set)
