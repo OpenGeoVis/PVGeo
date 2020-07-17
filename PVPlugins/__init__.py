5746b47cbecf727c696aac81eec28be27ac1d997
banesullivan <banesulli@gmail.com>
2020-07-16 20:39:26 -0400

    Remove imports from PVPlugins init

--- PVPlugins/__init__.py
+++ PVPlugins/__init__.py
@@ -2,15 +2,6 @@
 This package is a set of module files for each sub-package in PVGeo. Each file here
 contains the neccessary wrappings for PVGeo to be used directly in ParaView.
 """
-from .PVGeo_Filters import *
-from .PVGeo_Grid_Tools import *
-from .PVGeo_GSLib import *
-from .PVGeo_Model_Builder import *
-from .PVGeo_Readers import *
-from .PVGeo_Tunneling import *
-from .PVGeo_UBC_Tools import *
-import PVGeo_All
-
 
 __author__ = 'Bane Sullivan'
 __license__ = 'BSD-3-Clause'
