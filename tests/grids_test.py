877e2c5dae2af42ddf51a721dd241d9631e497a9
banesullivan <banesulli@gmail.com>
2020-09-29 22:55:20 -0600

    Improve ESRI grid test

--- tests/grids_test.py
+++ tests/grids_test.py
@@ -451,29 +451,7 @@ class TestEsriGridReader(TestBase):
     def setUp(self):
         TestBase.setUp(self)
         # Create a temporary directory
-        self.test_dir = tempfile.mkdtemp()
-        self.filename = os.path.join(self.test_dir, 'test.dat')
-        sample = """ncols         4
-nrows         6
-xllcorner     100.0
-yllcorner     50.0
-cellsize      50.0
-NODATA_value  -9999
--9999 -9999 5 2
--9999 20 100 36
-3 8 35 10
-32 42 50 6
-88 75 27 9
-13 5 1 -9999
-"""
-        with open(self.filename, 'w') as f:
-            f.write(sample)
-
-
-    def tearDown(self):
-        # Remove the test data directory after the test
-        shutil.rmtree(self.test_dir)
-        TestBase.tearDown(self)
+        self.filename = os.path.join(os.path.dirname(__file__), 'data', 'vd2.asc')
 
     ###########################################
 
@@ -486,14 +464,19 @@ NODATA_value  -9999
         # Test data object
         self.assertIsNotNone(img)
         nx, ny, nz = img.GetDimensions()
-        self.assertEqual(nx, 4)
-        self.assertEqual(ny, 6)
+        self.assertEqual(nx, 5)
+        self.assertEqual(ny, 9)
         dx, dy, dz = img.GetSpacing()
-        self.assertTrue(50.0 == dx == dy == dz)
+        self.assertTrue(100.0 == dx == dy == dz)
         ox, oy, oz = img.GetOrigin()
-        self.assertEqual(ox, 100.0)
-        self.assertEqual(oy, 50.0)
+        self.assertEqual(ox, 0.0)
+        self.assertEqual(oy, 0.0)
         self.assertEqual(oz, 0.0)
+        # Check that NaNs are correct
+        nans = np.argwhere(np.isnan(img["Data"]))
+        truth = np.array([[22], [27], [32]])
+        self.assertEqual(len(nans), 3)
+        self.assertTrue(np.allclose(nans, truth))
         return
 
     # def test_read_big(self):
