6637b0b11c20665617235b902e86ead97f4681e1
banesullivan <bane.sullivan@kitware.com>
2021-04-14 20:18:35 -0600

    Fix syntax issues

--- pvmacros/vis/axes.py
+++ pvmacros/vis/axes.py
@@ -19,13 +19,13 @@ def customAxisTicks(rng, axis=0, uniform=False):
     # note that third parameter is the step size
     # get the active view
     rv = GetActiveViewOrCreate('RenderView')
-    if axis is 0 or uniform:
+    if axis == 0 or uniform:
         rv.AxesGrid.XAxisUseCustomLabels = 1
         rv.AxesGrid.XAxisLabels = rng
-    if axis is 1 or uniform:
+    if axis == 1 or uniform:
         rv.AxesGrid.YAxisUseCustomLabels = 1
         rv.AxesGrid.YAxisLabels = rng
-    if axis is 2 or uniform:
+    if axis == 2 or uniform:
         rv.AxesGrid.ZAxisUseCustomLabels = 1
         rv.AxesGrid.ZAxisLabels = rng
     RenderAllViews()
@@ -45,13 +45,13 @@ def resetAxisTicks(axis):
     if not isinstance(axis, (list, tuple)):
         axis = [axis]
     for ax in axis:
-        if ax is 0:
+        if ax == 0:
             rv.AxesGrid.XAxisLabels = []
             rv.AxesGrid.XAxisUseCustomLabels = 0
-        if ax is 1:
+        if ax == 1:
             rv.AxesGrid.YAxisLabels = []
             rv.AxesGrid.YAxisUseCustomLabels = 0
-        if ax is 2:
+        if ax == 2:
             rv.AxesGrid.ZAxisLabels = []
             rv.AxesGrid.ZAxisUseCustomLabels = 0
         RenderAllViews()
