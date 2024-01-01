b6ac87622ad1096e3ce373f1eea7c3d8abd92906
Bane Sullivan <banesullivan@gmail.com>
2024-01-01 13:50:05 -0800

    Bump version: 2.1.1 → 3.0.0

--- PVGeo/__init__.py
+++ PVGeo/__init__.py
@@ -47,7 +47,7 @@ from .cmaps import *
 __author__ = 'Bane Sullivan'
 __license__ = 'BSD-3-Clause'
 __copyright__ = '2018, Bane Sullivan'
-__version__ = '2.1.1'
+__version__ = '3.0.0'
 __displayname__ = 'PVGeo'
 
 
